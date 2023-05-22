8a3ff247aed7db79e8d6fd112738d2b47bbe20cd
Lucas Catunda <lucas.catunda@cinbesa.com.br>
2023-05-22 12:07:40 -0300

    novos arquivos adicionados

--- src/POO/Person.java
+++ src/POO/Person.java
@@ -4,12 +4,18 @@ public class Person {
     private String name;
     private int age;
 
-    public Person(String initialName) {
-        this.age = 0;
+    public Person(String initialName, int idade) {
+        this.age = idade;
         this.name = initialName;
     }
 
     public void printPerson() {
         System.out.println(this.name + ", age " + this.age + " years");
+    }
+
+    public void growOlder(){
+        if(this.age < 30){
+            this.age = this.age + 1;
+        }
     }
 }
---
+++ src/decreasingExercise.java
@@ -0,0 +1,12 @@
+import POO.DiminuirContador;
+
+public class decreasingExercise{
+    public static void main(String[] args) {
+        DiminuirContador counter = new DiminuirContador(100);
+        counter.mostraValor();
+        counter.diminuir();
+        counter.mostraValor();
+        counter.resetar();
+        counter.mostraValor();
+    }
+}
