a3a0404e78cbe690f7906ab233da531a3046718c
Lucas Catunda <lucas.catunda@cinbesa.com.br>
2023-05-23 11:50:40 -0300

    Arquivos modificados

--- src/Classes/Person.java
+++ src/Classes/Person.java
@@ -3,14 +3,24 @@ package Classes;
 public class Person {
     private String name;
     private int age;
+    private double peso;
+    private double altura;
     //Constructor
     public Person(String initialName, int idade) {
         this.age = idade;
         this.name = initialName;
     }
-    //Printa informações
-    public void printPerson() {
-        System.out.println(this.name + ", age " + this.age + " years");
+
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura){
+        this.altura = altura ;
+    }
+
+    public double showImc(){
+        return this.peso / Math.pow(this.altura,2);
     }
     //Adiciona + 1 na this.age se a pessoa for menor que 30 anos
     public void growOlder(){
@@ -25,5 +35,13 @@ public class Person {
         }else{
             return false;
         }
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    @Override
+    public String toString(){
+        return this.name + ", tem " + this.age + " anos"; 
     }
 }
--- src/POO_Exercicios/pessoas.java
+++ src/POO_Exercicios/pessoas.java
@@ -4,7 +4,10 @@ import Classes.Person;
 public class pessoas {
     public static void main(String[] args) {
         Person lucas = new Person("Lucas", 27);
-        lucas.printPerson();
         System.out.println("Lucas é menor de idade? " + lucas.checkAge());
+        System.out.println(lucas);
+        lucas.setAltura(1.70);
+        lucas.setPeso(85);
+        System.out.println(String.format("%.2f",lucas.showImc()));
     }
 }
