b9519b93cb5fc93f8aa3bf7d1acf5a9c1fa85ab5
lucasrcz <lucasrczz.01@gmail.com>
2023-05-30 15:46:46 -0300

    Nova classe mensagem

--- src/fundamentos/message.java
+++ src/fundamentos/message.java
@@ -1,12 +1,35 @@
 package fundamentos;
-import java.util.Scanner;
+import java.util.ArrayList;
 
-public class message {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Escreva uma menssagem:");
-        String message = scanner.nextLine();
-        System.out.println(message);
-        scanner.close();
+public class Message {
+    
+    private String name;
+ 
+    public Message(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Message sms = new Message("SMS");
+        Message mms = new Message("MMS");
+        
+        System.out.println(sms.equals(sms));
+        System.out.println(sms.equals(mms));
+        
+        ArrayList<Message> messages = new ArrayList<>();
+        if (!messages.contains(sms)) {
+            messages.add(sms);
+        }
+        
+        if (!messages.contains(sms)) {
+            messages.add(sms);
+        }
+        
+        if (!messages.contains(new Message("SMS"))) {
+            messages.add(sms);
+        }
+        
+        System.out.println(messages.size());
     }
 }
+
