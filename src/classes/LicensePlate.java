80ff00bd15446ac7aa63d468b3666263d2000a54
Lucas Catunda <lucas.catunda@cinbesa.com.br>
2023-05-30 14:54:20 -0300

    novos arquivos

---
+++ src/classes/LicensePlate.java
@@ -0,0 +1,45 @@
+package classes;
+
+public class LicensePlate {
+
+    // these instance variables have been defined as final, meaning
+    // that once set, their value can't be changed
+    private final String liNumber;
+    private final String country;
+
+    public LicensePlate(String country, String liNumber) {
+        this.liNumber = liNumber;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return country + " " + liNumber;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        
+        if (!(object instanceof LicensePlate)) {
+            return false;
+        }
+        
+        LicensePlate compared = (LicensePlate) object;
+        
+        if (this.liNumber.equals(compared.liNumber) &&
+                this.country.equals(compared.country)) {
+            return true;
+        }        
+        
+        return false;
+    }
+
+    @Override
+    public int hashCode(){
+        return this.country.hashCode() + this.liNumber.hashCode();
+    }
+
+}
